911ff9666365572c5ea60b31f37806a0449bf05c
Dillon <dfcross18@gmail.com>
2024-08-14 19:41:04 -0700

    added tailwindcss

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,10 @@
+import './index.css'
 
 function App() {
   return (
     <div>
-      <header>
-        Hello
-      </header>
-    </div>
+      <p className="text-green-600 text-center font-bold text-8xl">hello</p>
+    </div>  
   );
 }
 
