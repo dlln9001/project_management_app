9292e5a65d43c3228a05badd0416b7b7aeb9f8e9
Dillon <dfcross18@gmail.com>
2024-09-22 13:05:41 -0700

    added testing in django for boards

--- boards/api/urls.py
+++ boards/api/urls.py
@@ -2,20 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get/', views.get_board),
-    path('change-board-name/', views.change_board_name),
-    path('change-board-description/', views.change_board_description),
-    path('delete-board/', views.delete_board),
-    path('create-group/', views.create_group),
-    path('get-groups/', views.get_groups),
-    path('create-item/', views.create_item),
-    path('edit-item/', views.edit_item),
-    path('delete-item/', views.delete_item),
-    path('edit-group-name/', views.edit_group_name),
-    path('delete-group/', views.delete_group),
-    path('edit-group-color/', views.edit_group_color),
-    path('create-column/', views.create_column),
-    path('edit-column-value/', views.edit_column_value),
-    path('delete-column/', views.delete_column), 
-    path('edit-column-name/', views.edit_column_name)
+    path('get/', views.get_board, name='get-board'),
+    path('change-board-name/', views.change_board_name, name='change-board-name'),
+    path('change-board-description/', views.change_board_description, name='change-board-description'),
+    path('delete-board/', views.delete_board, name='delete-board'),
+    path('create-group/', views.create_group, name='create-group'),
+    path('get-groups/', views.get_groups, name='get-groups'),
+    path('create-item/', views.create_item, name='create-item'),
+    path('edit-item/', views.edit_item, name='edit-item'),
+    path('delete-item/', views.delete_item, name='delete-item'),
+    path('edit-group-name/', views.edit_group_name, name='edit-group-name'),
+    path('delete-group/', views.delete_group, name='delete-group'),
+    path('edit-group-color/', views.edit_group_color, name='edit-group-color'),
+    path('create-column/', views.create_column, name='create-column'),
+    path('edit-column-value/', views.edit_column_value, name='edit-column-value'),
+    path('delete-column/', views.delete_column, name='delete-column'), 
+    path('edit-column-name/', views.edit_column_name, name='edit-column-name')
 ]
--- boards/tests.py
+++ boards/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from django.urls import reverse
+from user_authentication.models import User
+from .models import Board
+from .api.serializers import BoardFullSerializer
 
 # Create your tests here.
+
+class BoardTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.testUser = User.objects.create_user(username='test1234', password='1234')
+        self.board = Board.objects.create(user=self.testUser)
+
+    def test_get_board(self):
+        self.client.force_authenticate(user=self.testUser)
+        url = reverse('get-board')
+        expected_board_info = BoardFullSerializer(self.board)
+        response = self.client.post(url, {'board_id': self.board.id})
+        self.assertEqual(response.data['boardInfo'], expected_board_info.data)
+    
+    def test_change_board_description(self):
+        self.client.force_authenticate(user=self.testUser)
+        url = reverse('change-board-description')
+        expected_description = 'New Description'
+        response = self.client.post(url, {'board_id': self.board.id, 'board_description': expected_description})
+        self.board.refresh_from_db()
+        self.assertEqual(self.board.description, expected_description)
+
+    def test_change_board_name(self):
+        self.client.force_authenticate(user=self.testUser)
+        new_name = 'New Name'
+        url = reverse('change-board-name')
+        response = self.client.post(url, {'board_id': self.board.id, 'board_name': new_name}, format='json')
+        self.board.refresh_from_db()
+        self.assertEqual(self.board.name, new_name)
+    
+    def test_delete_board(self):
+        self.client.force_authenticate(user=self.testUser)
+        url = reverse('delete-board')
+        initial_count = Board.objects.count()
+        response = self.client.post(url, {'board_id': self.board.id}, format='json')
+        self.assertEqual(Board.objects.count(), initial_count - 1)
+        
