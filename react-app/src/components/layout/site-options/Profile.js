de054a9625dbf0cb2e609b4ab3111e950392b7af
Dillon <dfcross18@gmail.com>
2024-10-14 16:42:01 -0700

    made profile picture its own component, taken from the profile component

--- react-app/src/components/layout/site-options/Profile.js
+++ react-app/src/components/layout/site-options/Profile.js
@@ -1,61 +1,14 @@
 import ReactDOM from 'react-dom'
 import { IoIosClose } from "react-icons/io";
 import { CiMail } from "react-icons/ci";
-import { BsPersonFillAdd } from "react-icons/bs";
-import { MdOutlineFileUpload } from "react-icons/md";
-import { FiTrash } from "react-icons/fi";
 import {useState, useRef, useEffect} from 'react'
+import ProfilePicture from './ProfilePicture';
 
 function Profile(props) {
     const name = JSON.parse(localStorage.getItem('userInfo')).name
     const username = JSON.parse(localStorage.getItem('userInfo')).username
-    const token = JSON.parse(localStorage.getItem('userToken'))
-    const [showChangePfp, setShowChangePfp] = useState(false)
-    const profilePictureRef = useRef('')
+    const userToken = JSON.parse(localStorage.getItem('userToken'))
 
-    useEffect(() => {
-        document.addEventListener('click', handleDocumentClick)
-    }, [])
-
-    function handleDocumentClick(e) {
-        if (profilePictureRef.current && !profilePictureRef.current.contains(e.target)) {
-            setShowChangePfp(false)
-        }
-    }
-
-    function changePfp(e) {
-        const profile_picture = new FormData()
-        profile_picture.append('pfp_file', e.target.files[0])
-        fetch('http://127.0.0.1:8000/user/change-pfp/', {
-            method: 'POST',
-            headers: {
-                'Authorization': `Token ${token}`,
-            },
-            body: profile_picture
-        })
-        .then(res => res.json())
-        .then(data => {
-            localStorage.setItem('userInfo', JSON.stringify(data.user))
-            props.setPfpUrl('http://127.0.0.1:8000' + JSON.parse(localStorage.getItem('userInfo')).profile_picture)
-            setShowChangePfp(false)
-        })
-    }
-
-    function removePfp(e) {
-        fetch('http://127.0.0.1:8000/user/delete-pfp/', {
-            method: "DELETE",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Token ${token}`
-            }
-        })
-        .then(res => res.json())
-        .then(data => {
-            localStorage.setItem('userInfo', JSON.stringify(data.user))
-            props.setPfpUrl('')
-            setShowChangePfp(false)
-        })
-    }
 
     return document.getElementById('portal-root') ? ReactDOM.createPortal (
         <>
@@ -69,37 +22,7 @@ function Profile(props) {
                 </div>
                 <div className="border border-slate-300 m-5 h-60 rounded-lg p-7 flex">
                     <div className='flex'>
-                        <div className='flex flex-col items-center relative' ref={profilePictureRef}>
-                            <div className=' h-36 w-36 bg-white rounded-full flex items-center justify-center text-white font-medium relative cursor-pointer group'
-                                onClick={() => setShowChangePfp(true)}>
-                                {props.is_default_profile_picture 
-                                ? <p className=' text-7xl bg-slate-400 w-full h-full rounded-full flex justify-center items-center'>{name[0].toUpperCase()}</p>
-                                : <img src={props.pfpUrl} alt="" className=' rounded-full object-cover h-full w-full' />
-                                }
-                                <div className='absolute bg-black opacity-60 h-full w-full rounded-full invisible group-hover:visible'></div>
-                                <div className='absolute text-center flex flex-col justify-center items-center w-2/3 invisible group-hover:visible'>
-                                    <div className='text-6xl'>
-                                        <BsPersonFillAdd />
-                                    </div>
-                                    <p className='text-xs'>Change profile picture</p>
-                                </div>
-                            </div>
-                            {showChangePfp &&
-                                <div className='absolute bg-white shadow-all-sides top-32 rounded-md text-black w-44 p-1 flex flex-col gap-2'>
-                                    <form action="">
-                                        <label htmlFor="choose-profile-pic" className='flex gap-2 hover:bg-slate-100 cursor-pointer px-2 py-1 rounded-sm'>
-                                            <MdOutlineFileUpload />
-                                            <p className='text-xs'>Upload a profile picture</p>
-                                        </label>
-                                        <input type="file" accept='image/*' className='hidden' id='choose-profile-pic' onChange={changePfp}/>
-                                    </form>
-                                    <div className='flex gap-2 hover:bg-slate-100 cursor-pointer px-2 py-1 rounded-sm' onClick={removePfp}>
-                                        <FiTrash />
-                                        <p className='text-xs'>Remove profile picture</p>
-                                    </div>
-                                </div>
-                            }
-                        </div>
+                        <ProfilePicture userToken={userToken} is_default_profile_picture={props.is_default_profile_picture} pfpUrl={props.pfpUrl} setPfpUrl={props.setPfpUrl} name={name}/>
                         <div className="ml-5 border border-transparent hover:border-slate-300 rounded-sm px-2 py-[2px] font-medium text-3xl has-[:focus]:border-sky-600 h-fit">
                             <input type="text" className="focus:outline-none" value={name} />
                         </div>
---
+++ react-app/src/components/layout/site-options/ProfilePicture.js
@@ -0,0 +1,94 @@
+import {useState, useRef, useEffect} from 'react'
+import { BsPersonFillAdd } from "react-icons/bs";
+import { MdOutlineFileUpload } from "react-icons/md";
+import { FiTrash } from "react-icons/fi";
+
+function ProfilePicture(props) {
+    const [showChangePfp, setShowChangePfp] = useState(false)
+    const profilePictureRef = useRef('')
+
+
+    useEffect(() => {
+        document.addEventListener('click', handleDocumentClick)
+    }, [])
+
+    function handleDocumentClick(e) {
+        if (profilePictureRef.current && !profilePictureRef.current.contains(e.target)) {
+            setShowChangePfp(false)
+        }
+    }
+
+    function changePfp(e) {
+        const profile_picture = new FormData()
+        profile_picture.append('pfp_file', e.target.files[0])
+        fetch('http://127.0.0.1:8000/user/change-pfp/', {
+            method: 'POST',
+            headers: {
+                'Authorization': `Token ${props.userToken}`,
+            },
+            body: profile_picture
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (data.user) {
+                localStorage.setItem('userInfo', JSON.stringify(data.user))
+                props.setPfpUrl('http://127.0.0.1:8000' + JSON.parse(localStorage.getItem('userInfo')).profile_picture)
+                setShowChangePfp(false)
+            }
+        })
+    }
+
+    function removePfp(e) {
+        fetch('http://127.0.0.1:8000/user/delete-pfp/', {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${props.userToken}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (data.user) {
+                localStorage.setItem('userInfo', JSON.stringify(data.user))
+                props.setPfpUrl('')
+                setShowChangePfp(false)
+            }
+        })
+    }
+
+    return (
+        <div className='flex flex-col items-center relative' ref={profilePictureRef}>
+            <div className=' h-36 w-36 bg-white rounded-full flex items-center justify-center text-white font-medium relative cursor-pointer group'
+                onClick={() => setShowChangePfp(true)}>
+                {props.is_default_profile_picture 
+                ? <p className=' text-7xl bg-slate-400 w-full h-full rounded-full flex justify-center items-center'>{props.name[0].toUpperCase()}</p>
+                : <img src={props.pfpUrl} alt="" className=' rounded-full object-cover h-full w-full' />
+                }
+                <div className='absolute bg-black opacity-60 h-full w-full rounded-full invisible group-hover:visible'></div>
+                <div className='absolute text-center flex flex-col justify-center items-center w-2/3 invisible group-hover:visible'>
+                    <div className='text-6xl'>
+                        <BsPersonFillAdd />
+                    </div>
+                    <p className='text-xs'>Change profile picture</p>
+                </div>
+            </div>
+            {showChangePfp &&
+                <div className='absolute bg-white shadow-all-sides top-32 rounded-md text-black w-44 p-1 flex flex-col gap-2'>
+                    <form action="">
+                        <label htmlFor="choose-profile-pic" className='flex gap-2 hover:bg-slate-100 cursor-pointer px-2 py-1 rounded-sm'>
+                            <MdOutlineFileUpload />
+                            <p className='text-xs'>Upload a profile picture</p>
+                        </label>
+                        <input type="file" accept='image/*' className='hidden' id='choose-profile-pic' onChange={changePfp}/>
+                    </form>
+                    <div className='flex gap-2 hover:bg-slate-100 cursor-pointer px-2 py-1 rounded-sm' onClick={removePfp}>
+                        <FiTrash />
+                        <p className='text-xs'>Remove profile picture</p>
+                    </div>
+                </div>
+            }
+        </div>
+    )
+}
+
+export default ProfilePicture
