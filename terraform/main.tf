ddcb18724edd54af01e57f2f9b4c03c59a808bf4
Dillon <dfcross18@gmail.com>
2024-12-26 00:22:11 -0700

    added terraform configuration

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+terraform {
+    required_providers {
+        google = {
+            source = "hashicorp/google"
+            version = "6.8.0"
+        }
+    }
+
+    backend "gcs" {
+        bucket  = "project-management-app-terraform-bucket"
+    }
+}
+
+
+provider "google" {
+    project = var.project_id
+    region  = "us-central1"
+    zone    = "us-central1-c"
+}
+
+
+resource "google_compute_instance" "web-server" {
+    name = "test-vm-2"
+    machine_type = "e2-micro"
+
+    boot_disk {
+    initialize_params {
+        image = "debian-12-bookworm-v20241210"
+    }
+    }
+
+    network_interface {
+    network = "default"
+    }
+}
+
